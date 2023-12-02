0971c9e3d040a2204122917fe8ecd81e8d9590c9
EthanK100 <emklenda@gmail.com>
2023-12-01 19:11:25 -0500

    populated the template functions with code that returns the value passed in divided by two, and in case of an integer, the result is rounded.

--- Source.cpp
+++ Source.cpp
@@ -2,18 +2,20 @@
 // CIS 1202 501
 // December 1, 2023
 
+#include<cmath>
+
 using namespace std;
 
 template <typename T> // template for floats and doubles
 T half(T number)
 {
-	
+	return (number / 2);
 }
 
 template <int> // template for integers
 int half(int number)
 {
-
+	return round(static_cast<float>(number / 2));
 }
 
 
